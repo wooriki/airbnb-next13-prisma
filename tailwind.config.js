6f5f758730dcb0b51246c0193564e134fa2746b1
woori <dlsltlvmfl@naver.com>
2024-08-31 22:21:09 +0900

    initial build..

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,tsx,tsx}",
+    "./pages/**/*.{js,ts,tsx,tsx}",
+    "./components/**/*.{js,ts,tsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
