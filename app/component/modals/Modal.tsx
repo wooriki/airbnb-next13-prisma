37818b46b9812b8c52f0804427459dd386a07e83
woori <dlsltlvmfl@naver.com>
2024-09-02 23:57:47 +0900

    Navbar composition state ...

--- app/component/modals/Modal.tsx
+++ app/component/modals/Modal.tsx
@@ -81,9 +81,7 @@ const Modal: React.FC<ModalProps> = ({
             <div className="translate h-full lg:h-auto md:h-auto border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
               {/* {헤더 파트} */}
               <div className="flex items-center p-6 rounded-t justify-center relative border-b-[1px]">
-                <button className="p-1 border-0 hover:opacity-70 transition absolute left-9">
-                  <IoMdClose size={18} />
-                </button>
+                <button className="p-1 border-0 hover:opacity-70 transition absolute left-9"></button>
               </div>
             </div>
           </div>
