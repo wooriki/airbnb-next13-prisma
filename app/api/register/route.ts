b38ba04f565cc85af5e9d239afd6cf63c32eb615
woori <dlsltlvmfl@naver.com>
2024-09-05 21:42:32 +0900

    db connecting ...

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,3 +1,9 @@
+import bcrypt from "bcrypt";
+
 export async function POST(request: Request) {
   const body = await request.json();
+
+  const { email, name, password } = body;
+
+  const hashedPassword = await bcrypt.hash(password, 12);
 }
